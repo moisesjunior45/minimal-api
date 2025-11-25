06a10f2e1a33a36dbf8c33cf82a21f8b9c6d1793
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-11-25 19:07:51 -0300

    Validando administrador com login e senha no banco de dados

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using MinimalApi.Infraestrutura.Db;
 using MinimalApi.DTOs;
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options =>
 {
@@ -13,11 +17,13 @@ builder.Services.AddDbContext<DbContexto>(options =>
     );
 });
 
+var app = builder.Build();
+
 app.MapGet("/", () => "Olá pessoal");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if (administradorServico.Login(loginDTO) != null)
     {
         return Results.Ok("Login realizado com sucesso!");
     }
