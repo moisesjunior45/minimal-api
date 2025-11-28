3cfe447d583e277e92e5dfb8cffa950f2fdf547d
Moisés Júnior <moisesoliveirajr1@gmail.com>
2025-11-28 19:37:53 -0300

    GET para retornar um veiculo

--- Program.cs
+++ Program.cs
@@ -65,6 +65,14 @@ app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico
     var veiculos = veiculoServico.Todos(pagina);
     return Results.Ok(veiculos);
 }).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = veiculoServico.BuscaPorId(id);
+    if (veiculo == null) return Results.NotFound();
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
 #endregion
 
 
